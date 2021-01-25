09560c769b41576440d41846037e26a6009be126
Stephanie Core <stephanie.core@mortenson.com>
2021-01-24 20:34:52 -0600

    save while determining next step

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,26 @@
 print("Hello from PyBank")
+
+import os
+import csv
+
+# Path to resources folder and CSV
+csvpath = os.path.join("..", "Resources", "budget_data.csv")
+
+# Open CSV File
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+print (csvreader)
+
+# Read Header
+csv_header = next(csvreader)
+
+# Assign Variables
+TotalMonths = 0
+NetProfitLoss = 0
+AvgChange = 0
+GreatestIncrease = 0
+GreatestDecrease = 0 
+Previous = 0
+Current = 0
+Counter = 0
