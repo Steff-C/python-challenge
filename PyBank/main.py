07713b711d24d0327ce066d75dcd5fe73cba20d2
Stephanie Core <stephanie.core@mortenson.com>
2021-01-25 15:58:03 -0600

    Final with changes to skip header for correct totals

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,51 +3,58 @@ import csv
 
 # Path to resources folder and CSV
 budgetfile_path = os.path.join("Resources", "budget_data.csv")
-print(budgetfile_path)
+#   print(budgetfile_path)
 
 # Open CSV File and read header
 with open(budgetfile_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+#Read Header
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+#    print(f"CSV Header: {csv_header}")
 
 # Assign Variables    
-    TotalMonths = 0
-    TotalProfitLoss = 0
-    Difference = 0
-    GreatestIncrease = 0
+    TotalMonths =0
+    TotalProfitLoss =0
+    AvgDifference =0
+    GreatestIncrease =0
     GreatestIncreaseMonth = ""
     GreatestDecrease = 0 
     GreatestDecreaseMonth = ""
-    PreviousTotal = 0
-    CurrentTotal = 0
-    Counter = 0
-    Change = 0
-   
+    PreviousTotal =0
+    CurrentTotal =0
+    Counter =0
+    i =0
+    
     for row in csvreader:
-        Counter = Counter+1
-        Month = row[0]
-        profitloss = int(row[1])
+        i += 1
         TotalMonths = TotalMonths+1
-        TotalProfitLoss = TotalProfitLoss+profitloss
+        if i == 1:
+            PreviousTotal = int(row[1])
+            TotalProfitLoss = int(row[1])
+            Counter = Counter+1
+        else:
+        
               
 # Calculate Greatest Increase       
-        if profitloss>GreatestIncrease:
-            GreatestIncrease=profitloss
-            GreatestIncreaseMonth=Month
+            if GreatestIncrease < (int(row[1]) - PreviousTotal):
+                GreatestIncrease= (int(row[1]) - PreviousTotal)
+                GreatestIncreaseMonth = row[0]
         
 # Calculate Greatest Decrease
-        if profitloss<GreatestDecrease:
-            GreatestDecrease=profitloss
-            GreatestDecreaseMonth=Month
-        if Counter>1:
-            CurrentTotal=profitloss
-            Difference=Difference+(CurrentTotal-PreviousTotal)
-        PreviousTotal=profitloss
+            if GreatestDecrease > (int(row[1]) - PreviousTotal):
+                GreatestDecrease= (int(row[1]) -PreviousTotal)
+                GreatestDecreaseMonth = row[0]
+
+            AvgDifference += int(row[1])-PreviousTotal
+            TotalProfitLoss += int(row[1])
+            Counter = Counter + 1
+            PreviousTotal= int(row[1])
+
+Counter = Counter - 1        
 
 # Calculate Average Net Change
-AverageChange = Difference/(TotalMonths-1)
+AverageChange = AvgDifference/(Counter)
 
 
 
