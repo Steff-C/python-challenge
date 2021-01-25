68259bb1af9cf0c224377b622dbe31e8694f5c79
Stephanie Core <stephanie.core@mortenson.com>
2021-01-24 22:03:15 -0600

    Update changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,31 @@
-print("Hello from PyBank")
-
 import os
 import csv
 
 # Path to resources folder and CSV
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+budgetfile_path = os.path.join("Resources", "budget_data.csv")
+print(budgetfile_path)
 
-# Open CSV File
-with open(csvpath) as csvfile:
+# Open CSV File and read header
+with open(budgetfile_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-print (csvreader)
-
-# Read Header
-csv_header = next(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
 # Assign Variables
-TotalMonths = 0
-NetProfitLoss = 0
-AvgChange = 0
-GreatestIncrease = 0
-GreatestDecrease = 0 
-Previous = 0
-Current = 0
-Counter = 0
+    TotalMonths = 0
+    NetProfitLoss = 0
+    AvgChange = 0
+    GreatestIncrease = 0
+    GreatestDecrease = 0 
+    Previous = 0
+    Current = 0
+    Counter = 0
+
+    for row in csvreader:
+        Counter = Counter+1
+        month = row [0]
+        profitloss = int(row[1])
+        TotalMonths = TotalMonths+1
+        NetProfitLoss = NetProfitLoss + profitloss
+
